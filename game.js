5c5c5dd25e1f06751ac320b9a197b6f9ca79c9dc
Rupinder <rupinderk2001@gmail.com>
2023-12-05 20:40:43 +0530

    Coded up 2048, works with PC

--- game.js
+++ game.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () =>{
 const gridWidth = 4;
 rows = gridWidth;
 columns = gridWidth;
-const gameArray = [];
+var gameArray = [];
 
 function printCurrArray () {
     // Only for debug purposes
@@ -57,20 +57,42 @@ function createEmptyArray() {
 function readKey(e) {
     // USE WASD or Arrow keys <3
     if((e.keyCode === 37) || (e.keyCode === 65)) {
-        keyLeft();
-        introduceNewNumber();
-        displayInBox();
+        keyLeft();       
+        if (currentZeros().length != 0) {
+            introduceNewNumber();
+        }
     } else if ((e.keyCode === 38) || (e.keyCode === 87)) {
-        keyUp()
+        keyUp();
+        if (currentZeros().length != 0) {
+            introduceNewNumber();
+        }
     } else if ((e.keyCode === 39) || (e.keyCode === 68)) {
         keyRight();
-        introduceNewNumber();
-        displayInBox();
+        if (currentZeros().length != 0) {
+            introduceNewNumber();
+        }
     } else if ((e.keyCode === 40) || (e.keyCode === 83 )) {
-        keyDown()
+        keyDown();
+        if (currentZeros().length != 0) {
+            introduceNewNumber();
+        }
     }
+    displayInBox();
 };
-
+const colorMapping = {
+    0    : "#cdc0b4",
+    2    : "#eee4da",
+    4    : "#ede0c8",
+    8    : "#f2b179",
+    16   : "#f59563",
+    32   : "#f67c5f",
+    64   : "#f65d39",
+    128  : "#edcf72",
+    256  : "#f0cf4a",
+    512  : "#e4c02a",
+    1024 : "#ecc400",
+    2048 : "#ecc400"
+};
 
 function displayInBox() {
     for (let i = 0; i < rows; i++) {
@@ -80,25 +102,56 @@ function displayInBox() {
             } else {
                 document.getElementById("boxNum".concat(gridWidth*i+j)).innerHTML = gameArray[i][j];
             }
+            document.getElementById("boxNum".concat(gridWidth*i+j)).style.backgroundColor = (colorMapping[gameArray[i][j]]);
         }
     }   
 };
+
+function transposeArray(array, arrayLength){
+    var newArray = [];
+    for(var i = 0; i < array.length; i++){
+        newArray.push([]);
+    };
+
+    for(var i = 0; i < array.length; i++){
+        for(var j = 0; j < arrayLength; j++){
+            newArray[j].push(array[i][j]);
+        };
+    };
+
+    return newArray;
+}
 
 createEmptyArray();
 introduceNewNumber();
 introduceNewNumber();
 displayInBox();
-document.addEventListener('keyup',readKey);
+ document.addEventListener('keyup',readKey);
+
+
+function keyUp() {
+    let gameArrayTranspose = transposeArray(gameArray,gridWidth);
+    gameArrayTranspose = leftise(gameArrayTranspose);
+    gameArray = transposeArray(gameArrayTranspose,gridWidth);
+}
 
 function keyLeft () {
-    console.log("hi1")
-    if (currentZeros().length === 0) {
-        return;
-    }
-    console.log("hi2")
+    gameArray = leftise(gameArray);
+}
 
+function keyDown() {
+    let gameArrayTranspose = transposeArray(gameArray,gridWidth);
+    gameArrayTranspose = rightise(gameArrayTranspose);
+    gameArray = transposeArray(gameArrayTranspose,gridWidth);
+}
+
+function keyRight () {
+    gameArray=rightise(gameArray);
+}
+
+function leftise(arr) {
     for (let i = 0; i < rows; i++) {
-        let newRow = gameArray[i].filter(item => item !== 0);
+        let newRow = arr[i].filter(item => item !== 0);
         // console.log(newRow.length);
         // for(let newRoIn = 0; newRoIn < newRow.length;newRoIn++) {
         //     console.log(newRow[newRoIn]);
@@ -117,28 +170,17 @@ function keyLeft () {
         }
         for (let gi = 0; gi < gridWidth; gi++) {
             if (gi<newRow.length) {
-                gameArray[i][gi] = newRow[gi];
+                arr[i][gi] = newRow[gi];
             } else {
-                gameArray[i][gi] = 0;
+                arr[i][gi] = 0;
             }
         }
     }
-
+    return arr;
 }
-
-function keyRight () {
-    console.log("hi1")
-    if (currentZeros().length === 0) {
-        return;
-    }
-    console.log("hi2")
-
+function rightise(arr) {
     for (let i = 0; i < rows; i++) {
-        let newRow = gameArray[i].filter(item => item !== 0);
-        // console.log(newRow.length);
-        // for(let newRoIn = 0; newRoIn < newRow.length;newRoIn++) {
-        //     console.log(newRow[newRoIn]);
-        // }
+        let newRow = arr[i].filter(item => item !== 0);
         for (let j = newRow.length-1; j > 0 ; j--) {
             if(newRow[j] == newRow[j-1]) {
                 newRow[j]  *= 2;
@@ -153,14 +195,12 @@ function keyRight () {
         }
         for (let gi = 0; gi < gridWidth; gi++) {
             if (gi>=(gridWidth-newRow.length)) {
-                gameArray[i][gi] = newRow[gi-(gridWidth-newRow.length)];
+                arr[i][gi] = newRow[gi-(gridWidth-newRow.length)];
             } else {
-                gameArray[i][gi] = 0;
+                arr[i][gi] = 0;
             }
         }
     }
-
+    return arr;
 }
-
-
 })
